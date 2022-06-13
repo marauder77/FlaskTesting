318276edf9410d464834dab413ec304bf5570304
marauder77 <63757565+marauder77@users.noreply.github.com>
2022-06-13 02:04:51 -0700

    adding updates to game

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, render_template, redirect, flash, session
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, session, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
 
 from boggle import Boggle
 app = Flask(__name__)
 boggle_game = Boggle()
 
 app.config['SECRET_KEY'] = "2222"
-# debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
@@ -17,3 +17,23 @@ def homepage():
     nplays = session.get("nplays", 0)
 
     return render_template("index.html", board=board, highscore=highscore, nplays=nplays)
+
+# PERHAPS GO OVER THIS? COMPLETELY FORGOT ANYTHING RELATED TO JSON/AXIOS/AJAX
+@app.route('/check-word')
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+@app.route("/post-secure", methods=["POST"])
+def post_score():
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+    
+    return jsonify(brokeRecord=score > highscore)
